843c24d50a5b3525104ac718c01b934f44833005
vikbytes <55359319+vikbytes@users.noreply.github.com>
2025-08-12 19:16:21 +0200

    Upgraded to JVM 24 and Kotlin 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.2.0"
     application
     id("org.graalvm.buildtools.native") version "0.10.6"
 }
@@ -32,7 +32,7 @@ dependencies {
 
 tasks.test { useJUnitPlatform() }
 
-kotlin { jvmToolchain(21) }
+kotlin { jvmToolchain(24) }
 
 application {
     mainClass.set("com.vikbytes.MainKt")
